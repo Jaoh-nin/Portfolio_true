4aaca40c2fcae77e1735758fa97d77b1822fb014
Jaoh-nin <tanaput235@gmail.com>
2026-04-11 15:44:40 +0700

    new A2-17.py

---
+++ TOI-ZERO/A2-17.py
@@ -0,0 +1,28 @@
+#รับข้อมูลขนาดและรสชาติ
+order = input().split()
+size = str(order[0])
+flavour = str(order[1])
+
+#รับข้อมูลเครื่องเคียง
+plus = input().split
+side = str(plus[0])
+amount = int(plus[1])
+if amount == 0:
+  amount == 1
+
+plus1 = side * amount
+
+#หมวดราคาขนาด
+"S" = 40
+"M" = 60
+"L" = 80
+
+#หมวดราคาน้ำซุป
+"R" = 20
+"T" = 40
+
+#หมวดราคาเครื่องเคียง
+"P" = 15
+"E" = 10
+
+print(size + flavour + amount)
