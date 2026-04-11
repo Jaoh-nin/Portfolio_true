40cd6952d2c6a348b9d7696116bc79f533caefc5
Jaoh-nin <tanaput235@gmail.com>
2026-04-11 14:37:46 +0700

    change to toizero a2-15

--- TOI-ZERO/A.py
+++ TOI-ZERO/A.py
@@ -1,2 +1,9 @@
-A = 1
-print(A)
+amount = input().split()
+am_1 = int(amount[0])
+am_2 = int(amount[1])
+am_3 = int(amount[2])
+price = int(input())
+ovr = (am_1 * 2)+(am_2 * 2) * am_3
+
+print(ovr)
+print(price * ovr)
