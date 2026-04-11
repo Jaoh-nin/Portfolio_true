354ce2fa4152be9539ffa177069a6110fd509443
Jaoh-nin <tanaput235@gmail.com>
2026-04-11 15:45:05 +0700

    delete

--- TOI-ZERO/A.py
+++
@@ -1,9 +0,0 @@
-amount = input().split()
-am_1 = int(amount[0])
-am_2 = int(amount[1])
-am_3 = int(amount[2])
-price = int(input())
-ovr = (am_1 * 2)+(am_2 * 2) * am_3
-
-print(ovr)
-print(price * ovr)
